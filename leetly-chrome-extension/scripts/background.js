d528a4f6ef7ec2f9774118a0519c6767a2d7fe56
wayne-617 <wgl91603@uga.edu>
2025-06-02 22:59:36 -0400

    Detect leetcode submission script

--- leetly-chrome-extension/scripts/background.js
+++ leetly-chrome-extension/scripts/background.js
@@ -1,1 +1,14 @@
 // background script
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+    if (tab.url && tab.url.includes('leetcode.com') && tab.url.includes('/submissions/')) {
+        const urlParts = tab.url.split('/');
+        const urlParameters = new URLSearchParams(tab.url.split('?')[1]);
+        const name = urlParts[urlParts.length - 4];
+        const date = urlParameters.get("envId");
+
+        chrome.tabs.sendMessage(tabId, { type: "submission", name: name, date: date }, (response) => {
+            console.log('Message sent to content script');
+            console.log("Response from content script: ", response);
+        });
+    }
+})
--- leetly-chrome-extension/scripts/content.js
+++ leetly-chrome-extension/scripts/content.js
@@ -1,1 +1,12 @@
 // leetcode.com script
+
+(() => {
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const { type, name, date } = obj;
+
+        if (type === "submission") {
+            console.log("Received submission data:", { name, date });
+            response({ status: "success", message: `Received submission for ${name} on ${date}` });
+        }
+    });
+})();
