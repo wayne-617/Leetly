2d0a57fdb0c2711af0b1cff0e954a6a3f795a0ac
wayne-617 <wgl91603@uga.edu>
2025-06-10 22:39:32 -0400

    Base oauth listener

--- leetly-chrome-extension/scripts/background.js
+++ leetly-chrome-extension/scripts/background.js
@@ -12,3 +12,13 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
         });
     }
 })
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.type === "github-auth") {
+        const clientId = "Ov23liIOTLxZBviGZ6l3";
+        const redirectUri = chrome.identity.getRedirectURL();
+        const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=repo`;
+
+        console.log("Redirecting to GitHub OAuth:", authUrl);
+    }
+});
