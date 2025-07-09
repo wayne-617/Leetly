4607d692e7874b1609f66f1ac2352d01ecd6dc6e
wayne-617 <wgl91603@uga.edu>
2025-07-08 22:48:03 -0400

    Check for credentials

--- leetly-chrome-extension/scripts/content.js
+++ leetly-chrome-extension/scripts/content.js
@@ -7,6 +7,14 @@
         if (type === "submission") {
             console.log("Received submission data:", { name, date });
             response({ status: "success", message: `Received submission for ${name} on ${date}` });
+
+            chrome.storage.local.get(["githubAccessToken", "githubRepo"], ({ githubAccessToken, githubRepo }) => {
+                if (!githubAccessToken || !githubRepo) {
+                    console.error("GitHub credentials not found.");
+                    sendResponse({ status: "error", message: "GitHub not connected." });
+                    return;
+                }
+            });
         }
     });
 })();
