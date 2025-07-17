7d9af665a5771dbf006703515ded3d20db6ed0d8
wayne-617 <wgl91603@uga.edu>
2025-07-16 23:26:16 -0400

    Fetch params on submission

--- leetly-chrome-extension/scripts/background.js
+++ leetly-chrome-extension/scripts/background.js
@@ -39,17 +39,23 @@ function getSubmissionData() {
         console.log("No submission date found.");
     }
 
-
+    return {
+        code: code ? code.innerText : null,
+        problemTitle: problemTitle ? problemTitle.innerText : null,
+        codeLanguage: codeLanguage ? codeLanguage.lastChild.textContent.trim() : null,
+        submissionDate: submissionDate ? submissionDate.textContent.trim() : null
+    };
 }
 
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url && tab.url.includes('leetcode.com') && tab.url.includes('/submissions/')) {
         const urlParts = tab.url.split('/');
         const urlParameters = new URLSearchParams(tab.url.split('?')[1]);
-        const name = urlParts[urlParts.length - 4];
-        const date = urlParameters.get("envId");
+        params = getSubmissionData();
+        /* const name = urlParts[urlParts.length - 4];
+        const date = urlParameters.get("envId"); */
 
-        chrome.tabs.sendMessage(tabId, { type: "submission", name: name, date: date }, (response) => {
+        chrome.tabs.sendMessage(tabId, { type: "submission", ...params }, (response) => {
             console.log('Message sent to content script');
             console.log("Response from content script: ", response);
         });
--- leetly-chrome-extension/scripts/content.js
+++ leetly-chrome-extension/scripts/content.js
@@ -2,16 +2,16 @@
 
 (() => {
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, name, date } = obj;
+        const { type, code, problemTitle, codeLanguage, submissionDate } = obj;
 
         if (type === "submission") {
-            console.log("Received submission data:", { name, date });
-            response({ status: "success", message: `Received submission for ${name} on ${date}` });
+            console.log("Received submission data:", { code, problemTitle, codeLanguage, submissionDate });
+            response({ status: "success", message: `Received submission for ${problemTitle} on ${submissionDate}` });
 
             chrome.storage.local.get(["githubAccessToken", "githubRepo"], ({ githubAccessToken, githubRepo }) => {
                 if (!githubAccessToken || !githubRepo) {
                     console.error("GitHub credentials not found.");
-                    sendResponse({ status: "error", message: "GitHub not connected." });
+                    response({ status: "error", message: "GitHub not connected." });
                     return;
                 }
             });
